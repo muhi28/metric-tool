78f47765bfbcc5b757ae0ae5ce540a0dce1aa552
Muhamed Siljic <msiljic28@hotmail.com>
2019-07-25 15:56:37 +0200

    Add first y-channel psnr calculation

--- scripts/metric_calculation.py
+++ scripts/metric_calculation.py
@@ -15,12 +15,15 @@ def make_lut_v():
 
 
 def psnr(img1, img2):
-    mse = np.mean((img1 - img2) ** 2)
-    if mse == 0:
-        return math.inf
+    target_data = np.array(img1, dtype=np.float64)
+    ref_data = np.array(img2, dtype=np.float64)
 
-    pixel_max = 255.0
-    return 10 * math.log10((pixel_max * pixel_max) / mse)
+    diff = ref_data - target_data
+    diff = diff.flatten('C')
+
+    mse = math.sqrt(np.mean(diff ** 2.))
+
+    return 20 * math.log10(255 / mse)
 
 
 def checkVideoResolutions(raw_cap, coded_cap):
@@ -39,27 +42,29 @@ if __name__ == '__main__':
         print("Update the values -> raw video file/image and coded video file / image")
         exit(0)
 
+    # extract parameters
     rawFilePath = sys.argv[1]  # get raw file path
     codedFilePath = sys.argv[2]  # get coded file path
     colorSpaceType = sys.argv[3]  # get selected color space to calculate the metrics
-    metricToCalculate = sys.argv[4] # get selected metric to calculate
+    metricToCalculate = sys.argv[4]  # get selected metric to calculate
 
+    # initialize video capture for raw and coded video
     video_cap_raw = cv2.VideoCapture(rawFilePath)
     video_cap_coded = cv2.VideoCapture(codedFilePath)
 
-
-    # video_cap_raw = cv2.VideoCapture("E:\\HEVC_HM_Software\\software\\hm_360\\Tiling_Workflow_5g\\tiling_test_workflow\\test_hevc_tiles\\videos\\explore_the_world\\explore.mp4")
-    # video_cap_coded = cv2.VideoCapture("E:\\HEVC_HM_Software\\software\\hm_360\\Tiling_Workflow_5g\\tiling_test_workflow\\test_hevc_tiles\\videos\\explore_the_world\\explore_encoded.mp4")
+    # parse basename
     _, raw_file_basename = os.path.split(rawFilePath)
     _, coded_file_basename = os.path.split(codedFilePath)
+
 
     print("Selected color space -> {0}".format(colorSpaceType))
     print("Selected raw video file -> {0}".format(raw_file_basename))
     print("Selected coded video file -> {0}".format(coded_file_basename))
     print("FPS -> {0}".format(video_cap_coded.get(cv2.CAP_PROP_FPS)))
+    print("Color Space -> {0}".format(colorSpaceType))
     print("Selected Metric -> {0}".format(metricToCalculate))
 
-    count = 0
+    frameNumber = 0
     avg_psnr = 0
 
     frames = []  # create frame list used for our plotting part
@@ -78,8 +83,12 @@ if __name__ == '__main__':
     if not checkVideoResolutions(video_cap_raw, video_cap_coded):
         print("Video resolutions doesn't match")
 
+    psnrVal = 0
+
+    # start frame extraction and metric calculation
     while True:
 
+        # read frames
         raw_ret, raw_frame = video_cap_raw.read()
         cod_ret, coded_frame = video_cap_coded.read()
 
@@ -87,47 +96,62 @@ if __name__ == '__main__':
             break
 
         # check if the return value for frame capturing is false -> this means that we have reached the end of the video
-        if not raw_ret or not cod_ret or count == 300:
+        if not raw_ret or not cod_ret or frameNumber == 300:
             break
 
-        psnrVal = psnr(raw_frame, coded_frame)
+        # check selected color space
+        if colorSpaceType == "RGB":
+
+            psnrVal = psnr(raw_frame, coded_frame)
+
+            print("FRAME {0}".format(frameNumber))
+            print("PSNR-VALUE [RGB] -> {0:.2f} [dB] ".format(psnrVal))
+
+        elif colorSpaceType == "YUV":
+
+            # calculate Y-PSNR
+            yuvRaw = cv2.cvtColor(raw_frame, cv2.COLOR_BGR2YCrCb)
+            yuvCoded = cv2.cvtColor(coded_frame, cv2.COLOR_BGR2YCrCb)
+
+            # extract Y [Luminance] channel
+            y_raw, _, _ = cv2.split(yuvRaw)
+            y_coded, _, _ = cv2.split(yuvCoded)
+
+            # cv2.imshow("Y-Raw", y_raw)
+            # cv2.imshow("Y-Coded", y_coded)
+
+            # perform psnr for y-channel
+            psnrVal = psnr(yuvRaw, yuvCoded)
+            # upsnr = psnr(u_raw, u_coded)
+            # vpsnr = psnr(v_raw, v_coded)
+
+            # print current value
+            print("FRAME {0}".format(frameNumber))
+            print("PSNR-VALUE [Y-Channel] -> {0:.2f} [dB] ".format(psnrVal))
+
+
+        else:
+            print("Wrong color space.....")
+            exit(-1)
 
         # if there are black frames inside our test sequences then the psnr for them would be infinity
         # therefore we need to check if there are some infinity values to skip them
         if psnrVal == math.inf:
             continue
 
-        frames.append(count)
+        # append data to frames and psnr list
+        frames.append(frameNumber)
         psnrValues.append(psnrVal)
 
-        # print("FRAME {0}".format(count))
-        # print("PSNR-VALUE [RGB] -> {0:.2f} [dB] ".format(psnrVal))
-        # count += 1
-        # avg_psnr += psnrVal
+        frameNumber += 1
+        avg_psnr += psnrVal
 
-        lut_u = make_lut_u()
-        lut_v = make_lut_v()
-
-        test = cv2.cvtColor(raw_frame, cv2.COLOR_BGR2YUV)
-
-        raw_y, raw_u, raw_v = cv2.split(test)
-
-        raw_y = cv2.cvtColor(raw_y, cv2.COLOR_GRAY2BGR)
-        raw_u = cv2.cvtColor(raw_u, cv2.COLOR_GRAY2BGR)
-        raw_v = cv2.cvtColor(raw_v, cv2.COLOR_GRAY2BGR)
-
-        u = cv2.LUT(raw_u, lut_u)
-        v = cv2.LUT(raw_v, lut_v)
-
-        cv2.imshow("Y-Value", raw_y)
-        cv2.imshow("U-Value", u)
-        cv2.imshow("V-Value", v)
-
+    # release used video capture and destroy all opened windows
     video_cap_raw.release()
     video_cap_coded.release()
     cv2.destroyAllWindows()
 
-    print("AVG-PSNR-VALUE -> {0:.3f} [dB]".format(avg_psnr / count))
+    print("AVG-PSNR-VALUE -> {0:.3f} [dB]".format(avg_psnr / frameNumber))
 
     plt.plot(frames, psnrValues)
     plt.xlabel("Frame Number")
