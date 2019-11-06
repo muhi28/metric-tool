Implemented all necessary components. Need to fix some errors during vpsnr calculation.

utils/common_metrics.py
@@ -1,10 +1,23 @@
+import math
 from math import log10, inf, cos, pi
 from cv2.cv2 import split
 import skimage.measure
 import numpy as np
 
+from utils.vector_util import Vector3, get_yaw, get_pitch
 
 MAX_PIXEL = 255
+
+
+# _viewport = Viewport()
+#
+#
+# def set_viewport_settings(vp_width, vp_height, vp_fov_x):
+#     global _viewport
+#
+#     _viewport.set_width(vp_width)
+#     _viewport.set_height(vp_height)
+#     _viewport.set_fov_x(vp_fov_x)
 
 
 def calc_ssim(img1, img2, multi_channel=False):
@@ -75,8 +88,48 @@ def calc_wpsnr(img1, img2):
     return ((6 * y_psnr) + u_psnr + v_psnr) / 8.0
 
 
-def calc_vpsnr(img1, img2, head_motions):
+def calc_vpsnr(img1, img2, viewing_direction, _viewport):
+
     height, width = img1.shape[0], img1.shape[1]
+
+    y_raw, y_coded = img1, img2
+
+    sum = 0.0
+
+    vp_neg, vp_pos = (-1 * int(_viewport.get_width() / 2)), int(_viewport.get_width() / 2)
+
+    print(f"viewport range: {vp_neg} to {vp_pos}")
+
+    for i in range(vp_neg, vp_pos):
+
+        for j in range(vp_neg, vp_pos):
+            res = _viewport.get_spherical_coords(Vector3(i, j, 0), viewing_direction)
+
+            yaw = get_yaw(res)
+            pitch = get_pitch(res)
+
+            x_factor = width / 360.0
+            y_factor = height / 180.0
+
+            x = int(round(yaw * x_factor) % int(width))
+            y = int(round((pitch + 90) * y_factor) % int(height))
+
+            pixel_raw = int(y_raw[x][y])
+            pixel_coded = int(y_coded[x][y])
+
+            y_channel = int(pixel_raw - pixel_coded)
+
+            sum += y_channel * y_channel
+
+    _vpsnr_mse = (sum / (width * height))
+
+    if _vpsnr_mse == 0:
+        return inf
+
+    return 10.0 * np.log10((MAX_PIXEL * MAX_PIXEL) / _vpsnr_mse)
+
+
+
 
 
 

utils/vector_util.py
@@ -7,7 +7,7 @@ def _almost_equal(a, b, delta):
     return math.fabs(a - b) < delta
 
 
-def _get_pitch(vec3):
+def get_pitch(vec3):
     angle_v = Vector3(vec3.x, vec3.y, 0)
 
     degree = math.degrees(angle_v.angle(vec3))
@@ -16,7 +16,7 @@ def _get_pitch(vec3):
     return degree
 
 
-def _get_yaw(vector):
+def get_yaw(vector):
     angle_vec = Vector3(vector.x, vector.y, vector.z)
     x_axis = Vector3(1, 0, 0)
 
@@ -58,7 +58,7 @@ class Viewport:
         Used to create viewport necessary for viewport based psnr calculation
     """
 
-    def __init__(self, width, height, fov_x):
+    def __init__(self, width=0, height=0, fov_x=0):
         self._width = width
         self._height = height
         self._fov_x = fov_x
@@ -92,6 +92,9 @@ class Viewport:
         :return: none
         """
         self._height = height
+
+    def set_fov_x(self, fov):
+        self._fov_x = fov
 
     def get_fov_x(self):
         """
@@ -129,14 +132,14 @@ class Viewport:
         return mat3.inverse()
 
     def get_spherical_coords(self, viewport_coords, viewing_direction):
-        yaw = _get_yaw(viewing_direction)
-        pitch = _get_pitch(viewing_direction)
+        yaw = get_yaw(viewing_direction)
+        pitch = get_pitch(viewing_direction)
 
         rotation = _get_rotation_matrix(yaw, pitch, 0)
 
         adapt = Vector3(viewport_coords.x, viewport_coords.y, 1)
 
-        tmp = self.get_transform_2d_2_3d().transform()
+        tmp = self.get_transform_2d_2_3d().transform(adapt)
 
         res_vec = Vector3(tmp.z, tmp.x, tmp.y)
 
@@ -149,28 +152,28 @@ class Viewport:
 class Matrix3:
 
     def __init__(self, m00=0.0, m01=0.0, m02=0.0, m10=0.0, m11=0.0, m12=0.0, m20=0.0, m21=0.0, m22=0.0):
-        self.matrix = np.zeros((3, 3), dtype=np.float)
+        self.matrix = np.asmatrix(np.zeros((3, 3), np.float))
 
-        self.matrix[0][0] = m00
-        self.matrix[0][1] = m01
-        self.matrix[0][2] = m02
-        self.matrix[1][0] = m10
-        self.matrix[1][1] = m11
-        self.matrix[1][2] = m12
-        self.matrix[2][0] = m20
-        self.matrix[2][1] = m21
-        self.matrix[2][2] = m22
+        self.matrix[0, 0] = m00
+        self.matrix[0, 1] = m01
+        self.matrix[0, 2] = m02
+        self.matrix[1, 0] = m10
+        self.matrix[1, 1] = m11
+        self.matrix[1, 2] = m12
+        self.matrix[2, 0] = m20
+        self.matrix[2, 1] = m21
+        self.matrix[2, 2] = m22
 
     def multiply(self, m):
-        self.matrix = self.matrix.dot(m)
+        self.matrix.dot(m)
 
     def inverse(self):
-        return np.linalg.inv(self.matrix)
+        return self.matrix.getI()
 
     def transform(self, vec3):
-        return Vector3(self.matrix[0][0] * vec3.x + self.matrix[0][1] * vec3.y + self.matrix[0][2] * vec3.z,
-                       self.matrix[1][0] * vec3.x + self.matrix[1][1] * vec3.y + self.matrix[1][2] * vec3.z,
-                       self.matrix[2][0] * vec3.x + self.matrix[2][1] * vec3.y + self.matrix[2][2] * vec3.z)
+        return Vector3(self.matrix[0, 0] * vec3.x + self.matrix[0, 1] * vec3.y + self.matrix[0, 2] * vec3.z,
+                       self.matrix[1, 0] * vec3.x + self.matrix[1, 1] * vec3.y + self.matrix[1, 2] * vec3.z,
+                       self.matrix[2, 0] * vec3.x + self.matrix[2, 1] * vec3.y + self.matrix[2, 2] * vec3.z)
 
 
 class Vector3: